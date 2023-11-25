326cc4f5a442770fd7c259f4180260c3d48a5c5a
Pranay <pmundhra@wharton.upenn.edu>
2023-11-24 22:25:56 -0500

    getting started

--- src/BloomFilter.hs
+++ src/BloomFilter.hs
@@ -1,9 +1,63 @@
 module BloomFilter
-  ( someFunc,
+  (
   )
 where
 
-import HashFunction
+import Control.Applicative qualified as IntMap
+import Data.IntMap (IntMap)
+import HashFunction (Hash, genBoundedIntHasher)
+import System.Random (StdGen)
+import System.Random qualified as Random (mkStdGen, randomIO, uniform, uniformR)
 
-someFunc :: String
-someFunc = "Hello CIS 5520"
+mkStdGen :: Int -> StdGen
+mkStdGen = Random.mkStdGen . (* (3 :: Int) ^ (20 :: Int))
+
+-- TODO AN IMPORTANT DECISION: DO WE WANT TO USE A SET OR A MAP?
+
+data BloomFilter a = Filter
+  { max :: a,
+    internalMap :: IntMap Bool,
+    hashFunctions :: [Hash a]
+  }
+
+create :: [Hash a] -> BloomFilter a
+create = undefined
+
+-- empty bloomfilter
+
+fromList :: [Hash a] -> [a] -> BloomFilter a
+fromList = undefined
+
+insert :: a -> BloomFilter a
+insert = undefined
+
+refineHashFunction :: [Hash a] -> BloomFilter a -> BloomFilter a
+refineHashFunction = undefined
+
+-- add hash functions to existing bloom filter
+
+exists :: a -> Bool
+exists = undefined
+
+errorThreshold :: BloomFilter a -> [a] -> Double -> Bool
+errorThreshold = undefined
+
+-- this function takes in a bloom filter, a list of added elements, and
+-- an acceptable error threshold for false positives,
+-- and checks the probability of a false positive versus that threshold
+-- WE WANT TO PASS IN SOME FORM OF GENERATOR (USING RANDOM STATE)
+
+calibrateHashFunctions :: BloomFilter a -> Double -> Double -> BloomFilter a
+calibrateHashFunctions original sizeRatio = undefined
+
+-- take in a bloom filter and a size ratio > 1 (max ratio we are willing for the output
+-- bloom filter internal data structure vs elements put in). Then generate an appropriate
+-- number of hash functions randomly until our false positive error is below the
+-- error threshold
+-- THIS ALSO NEEDS A GENERATOR
+
+instance (Eq a) => Eq (BloomFilter a) where
+  (==) :: BloomFilter a -> BloomFilter a -> Bool
+  a == b = undefined
+
+-- EQUALITY WILL DEPEND ON THE INTERNAL DATA STRUCTURE
--- src/HashFunction.hs
+++ src/HashFunction.hs
@@ -1,12 +1,18 @@
 module HashFunction
   ( Hash,
+    genBoundedIntHasher,
   )
 where
 
+import Control.Monad qualified as Monad
 import Data.IntMap (IntMap)
-import Test.QuickCheck (Gen, chooseInt)
+import Test.QuickCheck (Gen, chooseInt, generate)
 
 data Hash a = Hasher {maxHashed :: Int, doHash :: a -> Int}
+
+instance Show (Hash a) where
+  show :: Hash a -> String
+  show (Hasher maxHashed _) = "Hasher with maxHashed = " ++ show maxHashed
 
 exampleHash :: Hash Int
 exampleHash = Hasher 6 (`mod` 7)
@@ -15,11 +21,36 @@ exampleHash = Hasher 6 (`mod` 7)
 -- 1
 
 -- >>> maxHashed exampleHash
--- 5
+-- 6
 
-genIntHasher :: Gen (Hash Int)
-genIntHasher = fmap (\x -> Hasher (x - 1) (`mod` x)) (chooseInt (1, 99))
+genBoundedIntHasher :: Int -> Gen (Hash Int)
+genBoundedIntHasher i =
+  Monad.liftM2
+    (\x y -> Hasher (i - 1) (\h -> mod (h * x + y) i))
+    (chooseInt (1, 99))
+    (chooseInt (1, 99))
 
-class HashedDataStructure a where
-  hashFunctions :: [Hash a]
-  internalDataStructure :: IntMap a
+-- >>> generate (genBoundedIntHasher 5)
+-- Hasher with maxHashed = 4
+
+customShow :: Hash Int -> String
+customShow (Hasher maxHashed doHash) =
+  "Hasher with maxHashed = "
+    ++ show maxHashed
+    ++ ",y mod i = "
+    ++ show (doHash 0)
+    ++ ",x mod i = "
+    ++ show (doHash 1 - doHash 0)
+
+demo :: Int -> Gen String
+demo i = do
+  x <- genBoundedIntHasher i
+  return (customShow x)
+
+-- >>> generate (demo 100000)
+-- "Hasher with maxHashed = 99999,y mod i = 7,x mod i = 96"
+
+-- for sufficiently large values input to demo, x and y mod i will simply be x and y.
+-- Note that the modulo here will not always be expressed in standard notation for Z/nZ,
+-- but will always be congruent to the correct value. No point in writing clunky code for the
+-- demo!
